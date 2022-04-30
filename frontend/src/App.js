5ee47b626ecc59bb0a21d25d9363f759b4e999d9
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2022-04-30 18:58:19 +0400

    create router/ start backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">G-express</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">G-express</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt",
+      category: "Shirts",
+      image:
+        "https://media.istockphoto.com/photos/mens-shirt-picture-id488160041?k=20&m=488160041&s=612x612&w=0&h=OH_-skyES8-aeTvDQHdVDZ6GKLsqp6adFJC8u6O6_UY=",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Adidas Fit shirt",
+      slug: "adidas-fit-shirt",
+      category: "Shirts",
+      image:
+        "https://media.istockphoto.com/photos/mens-shirt-picture-id488160041?k=20&m=488160041&s=612x612&w=0&h=OH_-skyES8-aeTvDQHdVDZ6GKLsqp6adFJC8u6O6_UY=",
+      price: 160,
+      countInStock: 10,
+      brand: "Adidas",
+      rating: 4.7,
+      numReviews: 12,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike fit shirt",
+      slug: "nike-fit-shirt",
+      category: "Shirts",
+      image:
+        "https://media.istockphoto.com/photos/mens-shirt-picture-id488160041?k=20&m=488160041&s=612x612&w=0&h=OH_-skyES8-aeTvDQHdVDZ6GKLsqp6adFJC8u6O6_UY=",
+      price: 10,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike Slim pant",
+      slug: "nike-slim-pant",
+      category: "Pants",
+      image:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/84fcbf8f-0d66-45fd-a5fa-a21cebe2398b/dri-fit-wild-run-phenom-elite-mens-woven-graphic-running-pants-3RllV6.png",
+      price: 60,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality pant",
+    },
+  ],
+};
+export default data;
