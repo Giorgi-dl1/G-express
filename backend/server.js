5fe72aa4c9f3e108830b86595fd7ae2ba11b30e4
Giorgi <drgls@mail.ru>
2023-03-21 20:34:53 +0400

    env variables replaced

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,47 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import path from "path";
-import seedRouter from "./routers/seedRoutes.js";
-import productRouter from "./routers/productRoutes.js";
-import userRouter from "./routers/userRouter.js";
-import orderRouter from "./routers/orderRoutes.js";
-dotenv.config();
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import path from 'path'
+import seedRouter from './routers/seedRoutes.js'
+import productRouter from './routers/productRoutes.js'
+import userRouter from './routers/userRouter.js'
+import orderRouter from './routers/orderRoutes.js'
+dotenv.config()
+const MONGODB_URI =
+  'mongodb+srv://visitor:visitor@cluster0.yedts.mongodb.net/G-express?retryWrites=true&w=majority'
 mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("connected to db"))
+  .then(() => console.log('connected to db'))
   .catch((err) => {
-    console.log(err.message);
-  });
-const app = express();
+    console.log(err.message)
+  })
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/api/keys/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
-});
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb')
+})
 
-app.use("/api/seed", seedRouter);
-app.use("/api/products", productRouter);
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
+app.use('/api/seed', seedRouter)
+app.use('/api/products', productRouter)
+app.use('/api/users', userRouter)
+app.use('/api/orders', orderRouter)
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
-);
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html')),
+)
 
 app.use((err, req, res, next) => {
-  res.status(500).send({ message: err.message });
-});
-const port = process.env.PORT || 5000;
+  res.status(500).send({ message: err.message })
+})
+const port = process.env.PORT || 5000
 app.listen(port, () => {
-  console.log(`app listening at port ${port}`);
-});
+  console.log(`app listening at port ${port}`)
+})
--- backend/utils.js
+++ backend/utils.js
@@ -1,4 +1,7 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = 'someSecret'
+
 export const generateToken = (user) => {
   return jwt.sign(
     {
@@ -7,24 +10,24 @@ export const generateToken = (user) => {
       isAdmin: user.isAdmin,
       _id: user._id,
     },
-    process.env.JWT_SECRET,
-    { expiresIn: "30d" }
-  );
-};
+    JWT_SECRET,
+    { expiresIn: '30d' },
+  )
+}
 
 export const isAuth = (req, res, next) => {
-  const authorization = req.headers.authorization;
+  const authorization = req.headers.authorization
   if (authorization) {
-    const token = authorization.slice(7, authorization.length);
-    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+    const token = authorization.slice(7, authorization.length)
+    jwt.verify(token, JWT_SECRET, (err, decode) => {
       if (err) {
-        res.status(401).send({ message: "Invalid Token" });
+        res.status(401).send({ message: 'Invalid Token' })
       } else {
-        req.user = decode;
-        next();
+        req.user = decode
+        next()
       }
-    });
+    })
   } else {
-    res.status(401).send({ message: "No Token" });
+    res.status(401).send({ message: 'No Token' })
   }
-};
+}
